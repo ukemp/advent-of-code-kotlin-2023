92836e6df8e89e964a51f30e3d0ede78f4fef49f
Ulrich Kemp <ulrich@kemp.in>
2023-12-06 07:56:30 +0100

    Day06

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,15 +1,11 @@
 
 data class Race(val time: Long, val distance: Long) {
 
-    fun computeWins(): Long {
-        var sum = 0L
-        for (speed in 1 until time) {
-            val distance = speed * (time - speed)
-            if (distance > this.distance) {
-                sum += 1
-            }
+    fun computeWins(): Int {
+        return (1 until time).count { speed ->
+            val travelled = speed * (time - speed)
+            travelled > this.distance
         }
-        return sum
     }
 }
 
@@ -23,7 +19,7 @@ fun parseRace(input: List<String>): List<Race> {
 }
 
 fun main() {
-    fun part1(input: List<String>): Long {
+    fun part1(input: List<String>): Int {
         val races = parseRace(input).also(::println)
         val wins = races.map { race ->
             race.computeWins()
@@ -32,14 +28,14 @@ fun main() {
         return wins
     }
 
-    fun part2(input: List<String>): Long {
+    fun part2(input: List<String>): Int {
         val race = Race(40709879L, 215105121471005L)
         return race.computeWins()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 288L)
+    check(part1(testInput) == 288)
 
     val input = readInput("Day06")
     part1(input).println()
